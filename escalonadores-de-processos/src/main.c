e6f548f30eb5287e348f8254122b26be517a66f1
Daniel Gustavo Favero <danielfavero17@gmail.com>
2023-04-24 20:13:39 -0300

    fix: menu de opcoes

--- escalonadores-de-processos/src/main.c
+++ escalonadores-de-processos/src/main.c
@@ -1,16 +1,16 @@
 #include "algorithms.h"
 
-int main(){
-    int opcao;
-    
-    printf("1) FCFS\n");
-    printf("2) SJF\n");
-    printf("3) Prioridades\n");
-    printf("4) RR\n");
-    printf("Opcao: ");
-    scanf("%d", &opcao);
+int main(){    
+    int opcao = 0;
 
     while(opcao != -1){
+        printf("1) FCFS\n");
+        printf("2) SJF\n");
+        printf("3) Prioridades\n");
+        printf("4) RR\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+        
         switch(opcao) {
             case 1:
                 executeFcfs();
